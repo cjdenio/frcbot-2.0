6e8a19299ed1f54a175d351708d7dabbfdf2d75a
Caleb Denio <cjdenio44@gmail.com>
2020-06-02 17:10:32 -0400

    Add awaits everywhere

--- src/index.ts
+++ src/index.ts
@@ -30,14 +30,16 @@ app.command("/frc", async ({ payload, ack, client }) => {
     case "team":
       let team = await tba.getTeam(parseInt(parsed.args[0]));
 
-      ack({
+      await ack({
         text: "",
         blocks: bk.team(team),
+        response_type: "in_channel"
       });
       break;
+    case "subscribe":
     case "watch":
-      ack();
-      client.views.open({
+      await ack();
+      await client.views.open({
         trigger_id: payload.trigger_id,
         view: bk.subscribeModal(),
       });
@@ -45,7 +47,7 @@ app.command("/frc", async ({ payload, ack, client }) => {
     case null:
     case "":
     default:
-      ack({
+      await ack({
         text: "",
         blocks: bk.help(),
       });
@@ -55,29 +57,29 @@ app.command("/frc", async ({ payload, ack, client }) => {
 
 // Event listeners
 app.event("app_home_opened", async ({ context, body, event }) => {
-  updateAppHome(context, event.user, body.team_id);
+  await updateAppHome(context, event.user, body.team_id);
 });
 
 // Block action listeners
 app.action("subscribe_event", async ({ ack, body, client }) => {
-  ack();
-  client.views.open({
+  await ack();
+  await client.views.open({
     trigger_id: (<BlockAction>body).trigger_id,
     view: bk.subscribeModal(),
   });
 });
 
 app.action("event_options", async ({ ack, body, client, context }) => {
-  ack();
+  await ack();
   await data.deleteSubscription(
     (<ButtonAction>(<BlockAction>body).actions[0]).value
   );
-  updateAppHome(context, body.user.id, body.team.id);
+  await updateAppHome(context, body.user.id, body.team.id);
 });
 
 // Shortcut listeners
 app.shortcut("subscribe_event", async ({ shortcut, ack, client }) => {
-  ack();
+  await ack();
   await client.views.open({
     trigger_id: shortcut.trigger_id,
     view: bk.subscribeModal(),
@@ -86,7 +88,7 @@ app.shortcut("subscribe_event", async ({ shortcut, ack, client }) => {
 
 // Modal submission listeners
 app.view("subscribe_event", async ({ view, ack, payload, context, body }) => {
-  ack();
+  await ack();
   const event = await tba.getEvent(view.state.values.event.event.value);
 
   await data.addSubscription(
@@ -94,7 +96,7 @@ app.view("subscribe_event", async ({ view, ack, payload, context, body }) => {
     view.state.values.channel.channel.selected_channel,
     event
   );
-  updateAppHome(context, body.user.id, body.team.id);
+  await updateAppHome(context, body.user.id, body.team.id);
 });
 
 async function updateAppHome(
