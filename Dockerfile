03008c54bc62b8d1a3779d69c094f7303e58316d
Caleb Denio <cjdenio44@gmail.com>
2020-06-03 14:41:18 -0400

    Add Docker image

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:latest
+
+WORKDIR /usr/src/app
+
+COPY . .
+
+RUN yarn install
+
+EXPOSE 3000
+
+CMD ["yarn", "dev"]
--- src/router.ts
+++ src/router.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("<h1>FRCBot 2.0</h1>");
 });
 
 export default router;
