2e4555c9365486db3711947adcb582028db1d48b
Caleb Denio <cjdenio44@gmail.com>
2020-06-08 17:43:57 -0400

    Add persistent data storage

--- src/ngrok/index.ts
+++ src/ngrok/index.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export async function getNgrokURL(): Promise<string> {
   const resp = await axios.get("http://ngrok:4040/api/tunnels");
-  return resp.data.tunnels[0].public_url;
+  return resp.data.tunnels.find((i) => i.proto == "https").public_url;
 }
